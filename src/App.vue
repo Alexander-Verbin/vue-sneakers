741e4bbcb41f69f10f1d3505548849fb98aed7dd
verbin.a <verbin.a@promail366.com>
2024-08-12 18:05:23 +0300

    add header layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
-<script setup>
-</script>
-
 <template>
-
+  <div class="w-4/5 mx-auto bg-white h-screen rounded-xl shadow-xl mt-14">
+    <Header/>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<script setup>
+  import Header from "@/components/Header.vue";
+</script>
